413ba642ea7b29da68565af30522b74211605633
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-06-01 02:03:42 +0200

    i called it 'nodeSHAPE' not 'nodePATH' converter >_<

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open('package.json') as fp:
   package = json.load(fp)
 
 setup(
-  name = 'houdini_nodepath_converter',
+  name = 'houdini_nodeshape_converter',
   version = '0.0.1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
-  py_modules = ['houdini_nodepath_converter'],
+  py_modules = ['houdini_nodeshape_converter'],
   install_requires = [x+y for x, y in package['python-dependencies'].items()],
   entry_points = {
     'console_scripts': [
-      'houdini-nodepath-converter=houdini_nodepath_converter:main'
+      'houdini-nodeshape-converter=houdini_nodeshape_converter:main'
     ]
   }
 )
